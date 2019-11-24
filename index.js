e51a58dfc1367ec13d58848ed6a5a4c942998b8a
Kyle Cheung <kcheu4@uwo.ca>
2019-11-24 14:07:15 -0500

    fixed centering on seat found/ seat not found text

--- index.js
+++ index.js
@@ -7,11 +7,8 @@ app.use(express.json());
 
 async function getSeat(first, last) {
     try {
-        console.log(first);
         for (const entry of db) {
             if (entry.first_name === first && entry.last_name === last) {
-                console.log('test');
-                console.log(`FOUND, ID ${entry.id}`);
                 return [entry.floor, entry.seat]
             }
         }
@@ -22,12 +19,16 @@ async function getSeat(first, last) {
 }
 
 app.post('/search', async (req, res) => {
-    console.log(`first name ${req.body.first}`);
-    console.log(`last name ${req.body.last}`);
     let result = await getSeat(req.body.first, req.body.last);
-    let location = { floor: result[0], seat: result[1] };
-    res.setHeader("Content-type", "application/json");
-    res.send(JSON.stringify({location}));
+
+    if (result === undefined) {
+        res.status(404).send({message: "seat not found" });
+    }
+    else {
+        let location = { floor: result[0], seat: result[1] };
+        res.setHeader("Content-type", "application/json");
+        res.send(JSON.stringify({location}));
+    }
 });
 
 app.use('/', express.static(path.join(__dirname + '/public')));
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,39 +1,81 @@
 async function getSeat(){
     let fragment = new DocumentFragment();
     let node = document.getElementById('seat');
+    let searchName = document.getElementById('input_name').value.split(" ");
+
     while(node.lastChild) {
         node.removeChild(node.lastChild);
     }
 
-    let searchName = document.getElementById('input_name').value.split(" ");
     try {
-        let response = await fetch('/search', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ 
-                first: searchName[0],
-                last: searchName[1]
-            })
-        });
 
-        const result = await response.json();
-        let element = []
-        for (let i = 0; i < 3; i++) {
-            let el = document.createElement('p');
-            el.className = 'seat-text';
-            element.push(el);
-
+        if (searchName.length !== 2) {
+            fragment = seatNotFound(searchName.join(" "));
         }
-        element[0].innerText = `${searchName[0]} is seated on`;
-        element[1].innerText = `Floor ${result.location.floor}`;
-        element[2].innerText = `Seat ${result.location.seat}`;
 
-        for (const el of element) { fragment.appendChild(el) };
-        node.appendChild(fragment);
-        console.log(response.status);
+        else {
+
+            let response = await fetch('/search', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ 
+                    first: searchName[0],
+                    last: searchName[1]
+                })
+            });
+
+            if (response.status === 200) {
+                const {location} = await response.json();
+                fragment = seatFound(searchName[0], location.floor, location.seat);
+            }
+
+            else {
+                fragment = seatNotFound(searchName.join(" "));
+            }
+        }
     } catch(e) {
         return e;
     }
+
+    finally {
+        node.appendChild(fragment);
+    }
+}
+
+function seatNotFound(name) {
+    let fragment = new DocumentFragment();
+    let error_el = document.createElement('p');
+    let name_el = document.createElement('p');
+
+    error_el.className = 'seat-text seat-text-fail';
+    name_el.className = 'seat-text seat-text-fail';
+
+    error_el.innerText = `Sorry, cannot find`;
+    name_el.innerText = `${name} in the system`;
+
+    fragment.appendChild(error_el);
+    fragment.appendChild(name_el);
+
+    return fragment;
+}
+
+function seatFound(name, floor, seat){
+    let fragment = new DocumentFragment();
+    let element = []
+
+    for (let i = 0; i < 3; i++) {
+        let el = document.createElement('p');
+        el.className = 'seat-text seat-text-success';
+        element.push(el);
+
+    }
+    element[0].innerText = `${name} is seated on`;
+    element[1].innerText = `Floor ${floor}`;
+    element[2].innerText = `Seat ${seat}`;
+
+    for (const el of element) { fragment.appendChild(el) };
+
+    return fragment;
 }
